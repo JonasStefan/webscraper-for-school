b90b2842fefd54eab194b5f3d3af38c8a39a3518
Nozino1 <jonas.j.stefan@gmail.com>
2022-10-31 13:41:52 +0100

    probably final commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-import requests_html
+import PySimpleGUI as pg
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,7 +7,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+listTestsThisWeek = []
+listTestsNextWeek = []
 url = "https://klio.webuntis.com"
 path = "C:\Program Files (x86)/chromedriver.exe"
 options = Options()
@@ -21,9 +25,10 @@ time.sleep(1.0)
 schoolParent = driver.find_element(By.CLASS_NAME, "Select-input")
 schoolInput = schoolParent.find_elements(By.XPATH, "*")[0]
 schoolInput.send_keys("HTBLA Mössingerstr.")
-time.sleep(0.6)
-schoolInput.send_keys(Keys.RETURN)
 time.sleep(1.0)
+schoolInput.send_keys(Keys.RETURN)
+time.sleep(0.8)
+
 userInputs = driver.find_elements(By.CLASS_NAME, "un-input-group__input")
 for element in userInputs:
     if element.get_attribute("type") == "text":
@@ -32,23 +37,59 @@ for element in userInputs:
         element.send_keys("htlMeinzh3f!")
         element.send_keys(Keys.RETURN)
 time.sleep(1.0)
+
 buttons = driver.find_elements(By.CLASS_NAME, "item-container")
 time.sleep(0.5)
 for element in buttons:
     if element.text == "Mein Stundenplan":
         element.click()
+        time.sleep(1.0)
+
+WebDriverWait(driver, 60).until(EC.frame_to_be_available_and_switch_to_it((By.ID,"embedded-webuntis")))
+time.sleep(0.5)
+legend = driver.find_elements(By.CLASS_NAME, "un-timetable-legend__cell")
+for element in legend:
+    if element.text == "Prüfung":
+        testColor = element.value_of_css_property("background-color")[:16]
+lessons = driver.find_elements(By.CLASS_NAME, "renderedEntry")
+for element in lessons:
+    if element.value_of_css_property("background-color")[:16] == testColor:
+        listTestsThisWeek.append(element.find_element(By.XPATH, "./div/table/tbody/tr[2]/td/table/tbody/tr[2]/td[1]/span").get_attribute("innerHTML"))
+buttons = driver.find_elements(By.CSS_SELECTOR, "button[type=button]")
+for element in buttons:                                  
+    if element.get_attribute("class") == "btn btn-default":
+        if element.find_element(By.XPATH, "./*").get_attribute("class") == "un-icon fa fa-arrow-right":
+            element.click()
 time.sleep(1.5)
-thisWeek = bs(driver.page_source, "html.parser")
-test = driver.find_elements(By.CLASS_NAME, "un-timetable__dnd-avatar un-timetable__dnd-avatar--hidden")
-print(test)
-# time.sleep(5.0)
-# buttons = driver.find_elements(By.CSS_SELECTOR, "button[type=button]")
-# for element in buttons:
-#     print(element.get_attribute("class"))                                     come back later...
-#     if element.get_attribute("class") == "btn btn-default":
-#         print("should work...")
-time.sleep(6.0)
+lessons = driver.find_elements(By.CLASS_NAME, "renderedEntry")
+for element in lessons:
+    if element.value_of_css_property("background-color")[:16] == testColor:
+        listTestsNextWeek.append(element.find_element(By.XPATH, "./div/table/tbody/tr[2]/td/table/tbody/tr[2]/td[1]/span").get_attribute("innerHTML"))
 
-###############################################################################################################################
+testsThisWeek = ""
+for element in listTestsThisWeek:
+    testsThisWeek = testsThisWeek + element + ", "
+testsThisWeek = testsThisWeek[:-2]
 
-print(thisWeek.prettify())
+testsNextWeek = ""
+for element in listTestsNextWeek:
+    testsNextWeek = testsNextWeek + element + ", "
+testsNextWeek = testsNextWeek[:-2]
+
+pg.theme("DarkBlue14")
+
+layout = [
+    [pg.Text("Tests in dieser Woche:")],
+    [pg.Text(str(testsThisWeek))],
+    [pg.Text("Tests in der nächsten Woche:")],
+    [pg.Text(str(testsNextWeek))],
+    [pg.Button("exit", size=(20, 2))]
+]
+
+window = pg.Window("pop-up", layout, size=(250, int(layout.__len__()) * 35), element_justification="l")
+while True:
+    event , values = window.read()
+    match event:
+        case "exit":
+            window.close()
+            break
